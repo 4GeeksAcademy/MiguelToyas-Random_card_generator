5ef7dc28a52acee728949afd04e552aee805e73a
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-07-03 19:03:43 +0000

    HTML y CSS terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+//Definición de Arrays
+const cardNumber = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+const cardSuit = ['']
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//Encontrar elementos en el DOM
+const naipe = document.getElementById("naipe");
+const number = document.getElementById("numero");
+const numberText = document.getElementById("textoNumero");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Función para ajustar el tamaño de fuente
+const adjustTextSize = () => {
+  const naipeWidth = naipe.offsetWidth;
+  const naipeHeight = naipe.offsetHeight;
+
+  // Calcular tamaño de fuente basado en las dimensiones de naipe
+  const fontSize = Math.min(naipeWidth, naipeHeight) / 2; // Ajusta el factor según necesites
+
+  numberText.style.fontSize = `${fontSize}px`;
 };
+
+// Llamar a la función inicialmente y en el resize del navegador
+adjustTextSize();
+window.addEventListener("resize", adjustTextSize);
+naipe.addEventListener("change", adjustTextSize);
