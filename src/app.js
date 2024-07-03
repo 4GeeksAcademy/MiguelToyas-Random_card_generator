ad63b149c1d51e8bb0144db05c46580e5b33a5a2
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-07-03 19:41:13 +0000

    Función crear carta funciona y botón funciona

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-//Definición de Arrays
-const cardNumber = [
+
+// Definición de Arrays
+const cardNumbers = [
   "2",
   "3",
   "4",
@@ -15,14 +16,21 @@ const cardNumber = [
   "J",
   "Q",
   "K",
-  "A",
+  "A"
 ];
-const cardSuit = ['']
+const cardSuits = [
+  "src/images/clubs.jpg",
+  "src/images/diamonds.jpg",
+  "src/images/hearts.jpg",
+  "src/images/spades.jpg"
+];
 
-//Encontrar elementos en el DOM
+// Encontrar elementos en el DOM
 const naipe = document.getElementById("naipe");
-const number = document.getElementById("numero");
+const number = document.getElementById("textoNumero");
 const numberText = document.getElementById("textoNumero");
+const suitUp = document.getElementById("suitUp");
+const suitDown = document.getElementById("suitDown");
 
 // Función para ajustar el tamaño de fuente
 const adjustTextSize = () => {
@@ -39,3 +47,30 @@ const adjustTextSize = () => {
 adjustTextSize();
 window.addEventListener("resize", adjustTextSize);
 naipe.addEventListener("change", adjustTextSize);
+
+// Generar una carta aleatoria
+const obtainRandomElement = arr => arr[Math.floor(Math.random() * arr.length)];
+
+function generateRandomCard() {
+  let cardNumber = obtainRandomElement(cardNumbers);
+  let cardSuit = obtainRandomElement(cardSuits);
+
+  number.innerHTML = cardNumber;
+  suitUp.src = cardSuit;
+  suitDown.src = cardSuit;
+
+  if (
+    cardSuit === "src/images/diamonds.jpg" ||
+    cardSuit === "src/images/hearts.jpg"
+  ) {
+    number.style.color = "red";
+  } else {
+    number.style.color = "black";
+  }
+}
+
+window.onload = () => {
+  generateRandomCard();
+};
+
+document.getElementById("button").addEventListener("click", generateRandomCard);
